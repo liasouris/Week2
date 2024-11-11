18f349ec82449d78d4338739c0e165dc6f2cfb3a
liasouris <lia.souris@student.lut.fi>
2024-11-11 18:40:11 +0200

    typos fixed

--- app.ts
+++ app.ts
@@ -1,83 +1,80 @@
-console.log("Hello world!");
+console.log("Hello World!"); 
 
-type TVechile = {
-    model: string,
-    color: string,
-    year: number,
-    power: number,
-}
+type TVehicle = {
+  model: string;
+  color: string;
+  year: number;
+  power: number;
+};
 
-let vehicle: TVechile = {
-    model: "Boring generic vehicle",
-    color: "Red",
-    year: 1993,
-    power: 60,
-}
+let vehicle: TVehicle = {
+  model: "Boring generic vehicle",
+  color: "Red",
+  year: 1993,
+  power: 60
+};
 
 console.log(vehicle);
 
 interface IVehicle {
-    model: string,
-    color: string,
-    year: number,
-    power: number,
+  model: string;
+  color: string;
+  year: number;
+  power: number;
 }
 
 interface ICar extends IVehicle {
-    bodyType: string,
-    wheelCount?: number,
-}
-
-let car: ICar = {
-    model: "Ford focus",
-    color: "Green",
-    year: 2016,
-    power: 150,
-    bodyType: "Hatchback",
-    wheelCount: 4,
+  bodyType: string;
+  wheelCount?: number;
 }
 
 interface IBoat extends IVehicle {
-    draft: number,
-}
-
-let boat: IBoat = {
-    model: "Bella",
-    color: "Black",
-    year: 2022,
-    power: 100,
-    draft: 0.42,
+  draft: number;
 }
 
 interface IPlane extends IVehicle {
-    wingspan: number,
+  wingspan: number;
 }
 
+let car: ICar = {
+  model: "Ford focus",
+  color: "Green",
+  year: 2016,
+  power: 150,
+  bodyType: "Hatchback",
+  wheelCount: 4
+};
+
+let boat: IBoat = {
+  model: "Bella",
+  color: "Black",
+  year: 2022,
+  power: 100,
+  draft: 0.42
+};
+
 let plane: IPlane = {
-    model: "Boeing 777",
-    color: "White",
-    year: 2020,
-    power: 170000,
-    wingspan: 65,
-}
+  model: "Boeing 777",
+  color: "White",
+  year: 2020,
+  power: 170000,
+  wingspan: 65
+};
 
 console.log(car);
 console.log(boat);
 console.log(plane);
 
-
-
 class VehicleService<T> {
-    private items: T[] = [];
-    
-    add(item: T): void {
-        this.items.push(item);
-    }
+  private items: T[] = [];
 
-    list(): T[] {
-        return this.items;
-    }
-    
+  add(item: T): void {
+    this.items.push(item);
+  }
+
+  list(): T[] {
+    return this.items;
+  }
 }
 
 let cars = new VehicleService<ICar>();
@@ -87,3 +84,4 @@ cars.add(car);
 boats.add(boat);
 
 console.log(cars.list());
+console.log(boats.list());
